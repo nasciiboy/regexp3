3267a74a1ad568362c540ee1665ce011f49b497c
nasciiboy <nasciiboy@gmail.com>
2017-02-11 12:54:09 -0600

    fuck you gofmt!

--- charUtils.go
+++ charUtils.go
@@ -8,24 +8,24 @@ func isAlnum( c rune ) bool { return isAlpha( c ) || isDigit( c ) }
 func isSpace( c rune ) bool { return c == ' ' || (c >= '\t' && c <= '\r') }
 
 func toLower( c rune ) rune {
-	if isLower( c ) { return c + 32 }
+  if isLower( c ) { return c + 32 }
 
-	return c
+  return c
 }
 
 
 func strChr( str string, r rune ) int {
   for i, c := range str {
     if c == r { return  i }
-	}
+  }
 
-	return -1
+  return -1
 }
 
 func strnchr( str string, v rune ) bool {
   for _, c := range( str) {
     if c == v { return true }
-	}
+  }
 
   return false
 }
@@ -37,15 +37,15 @@ func cmpChrCommunist( a, b rune ) bool {
 func findRuneCommunist( str string, chr rune ) bool {
   for _, c := range str {
     if cmpChrCommunist( c, chr ) == false { return false }
-	}
+  }
 
   return true;
 }
 
 func strnEqlCommunist( s, t string, n int ) bool {
   for i := 0; i < n; i++ {
-		if cmpChrCommunist( rune(s[i]), rune(t[i]) ) == false  { return false }
-	}
+    if cmpChrCommunist( rune(s[i]), rune(t[i]) ) == false  { return false }
+  }
 
   return true;
 }
@@ -54,16 +54,16 @@ func aToi( str string ) ( number uint32 ) {
   for _, c := range str {
     if isDigit( c ) == false { return }
 
-		number = 10 * number + ( uint32(c) - '0' )
-	}
+    number = 10 * number + ( uint32(c) - '0' )
+  }
 
-	return
+  return
 }
 
 func countCharDigits( str string ) int {
   for i, c := range str {
     if isDigit( c ) == false { return i }
-	}
+  }
 
-	return len( str )
+  return len( str )
 }
